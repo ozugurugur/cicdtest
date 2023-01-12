22205269b408830c04735f4fc11daff530266461
uğur özuğur <ugurozugur@ugurs-MacBook-Air.local>
2023-01-13 00:54:00 +0300

    2

--- function1/cloud-function/main.py
+++ function1/cloud-function/main.py
@@ -19,5 +19,5 @@ def hello_http(request):
    elif request_args and 'name' in request_args:
        name = request_args['name']
    else:
-       name = 'World'
+       name = 'Function1'
    return 'Hello {}!'.format(name)
